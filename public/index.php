9a9a928dc65e518a7c99a0880d0eadce0a21c21d
TahmidSadat02 <githubsadat@gmail.com>
2025-09-19 17:04:41 +0600

    Add public folder with assets, CSS, and main index.php

---
+++ public/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <Title>
+        Home | Online Classroom Managemnet
+    </Title>
+    <link rel="stylesheet" href="../public/css/style.css">
+</head>
+
+<body>
+    <div class="container">
+        <header>
+            <h1>Online Classroom Management System</h1>
+        </header>
+
+        <main>
+            <div class="welcome-section">
+                <h2>Welcome to the Online Classroom</h2>
+                <p>Manage your classes, assignments, and students all in one place.</p>
+                <div class=action-buttons>
+                    <a href="../app/views/auth/login.php" class="btn btn-login">Login</a>
+                    <a href="../app/views/auth/register.php" class="btn btn-register">Register</a>
+
+                </div>
+            </div>
+        </main>
+    </div>
+</body>
+
+</html>
